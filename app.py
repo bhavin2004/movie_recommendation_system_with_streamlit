58274afda5423de98020b07e676fedf846ab49c5
Bhavin Karangia <omkarpro6@gmail.com>
2025-01-14 12:21:10 +0530

    fetch poster function added

--- app.py
+++ app.py
@@ -9,32 +9,33 @@ from src.utlis import fetch_poster
 import pandas as pd
 import os
 
-# Check if similarity.pkl exists, if not, run the training pipeline
-if not os.path.exists('artifacts/similarity.pkl'):
-    train_pipe_obj = Training_Pipeline()
-    train_pipe_obj.run_pipeline()
+if (not os.path.exists('artifacts/similarity.pkl')):
+            train_pipe_obj = Training_Pipeline()
+            train_pipe_obj.run_pipeline()
+        
 
-# Load the processed data
 df = pd.read_csv('artifacts/processed_data/data.csv')
 
-# Set the app title
-st.title("Movie Recommendation System")
+st.title("Movie Recommandation System")
 
-# Movie selection dropdown
+
 movie = st.selectbox(
-    "Select a Movie from Below:",
-    df['title'].to_list()
+    "Select The Movie from Below:-",
+    (df['title'].to_list())
 )
 
-# Recommendation button
-if st.button("Recommend the Movies"):
-    predict_obj = PredictionPipeline()
-    recommended_movies = predict_obj.run_pipeline(movie=movie)
-    movie_id = list(recommended_movies.keys())
-    movie_name = list(recommended_movies.values())
 
-    # Display recommended movies in a row
-    for index, col in enumerate(st.columns(5, gap='medium')):
+if st.button("Recommand the Movies"):
+    predict_obj = PredictionPipeline()
+    recommended_movies=predict_obj.run_pipeline(movie=movie)
+    # for movie_id,movie_name in recommended_movies.items():
+    movie_id=list(recommended_movies.keys())
+    movie_name = list(recommended_movies.values())
+    for index,col in enumerate(st.columns(5,gap='medium')):
         with col:
-            st.image(fetch_poster(movie_id=movie_id[index]), use_container_width=True)
-            st.write(f"<p style='text-align: center;'>{movie_name[index]}</p>", unsafe_allow_html=True)
+            st.image(fetch_poster(movie_id=movie_id[index]))
+            st.write(movie_name[index])
+    # st.write(recommended_movies)
+    # st.write(type(recommended_movies))
+    # for i in recommended_movies:
+    #     st.write(i)
