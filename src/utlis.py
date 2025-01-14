ae08852b3803dfece1addc9f84953a87407cf620
Bhavin Karangia <omkarpro6@gmail.com>
2025-01-14 11:47:17 +0530

    fetch poster function added

--- src/utlis.py
+++ src/utlis.py
@@ -73,7 +73,7 @@ def fetch_poster(movie_id:int):
     print("hi2")
     data = responese.json()
     print("hi3")
-    return 'hi'  
+    return (data['postar_path'])    
 
 def load_pkl(pkl_path):
     try:
